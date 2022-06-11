727f5a5e78d299c54ddb0681aca8b96bf238cac5
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-06-11 12:20:13 -0600

    Create travel class, repository, test file, and sample data file

---
+++ src/Traveler.js
@@ -0,0 +1,10 @@
+class Traveler {
+    constructor(travelerData) {
+        this.id = travelerData.id;
+        this.name = travelerData.name;
+        this.travelerType = travelerData.travelerType;
+    }
+
+}
+
+"id":1,"name":"Ham Leadbeater","travelerType":"relaxer"
---
+++ test/Traveler-test.js
@@ -0,0 +1,19 @@
+import chai from 'chai';
+import Traveler from '../src/Traveler';
+import  travelerData  from '../src/data/sampleData-traveler';
+const expect = chai.expect;
+
+describe('Traveler', function() {
+    let newTraveler;
+    let newTraveler1;
+    this.beforeEach(() => {
+        newTraveler = new Traveler(travelerData[0])
+        newTraveler1 = new Traveler(travelerData[1])
+    })
+  it('should be a function', function() {
+    expect(Traveler).to.be.a('function');
+  });
+  it('should instanciate a new traveler', function() {
+    expect(newTraveler).to.instanceOf(Traveler);
+  });
+});
