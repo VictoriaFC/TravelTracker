08351d5820bcde47b386177a9b00259c02d68332
Victoria Fox-Collis <victoriafoxc@gmail.com>
2022-06-12 16:51:56 -0600

    Update fetch get function to arrow function and moved export to bottom of file

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,5 +1,27 @@
-export let fetchAll = (dataType) => {
+// fetch GET
+
+const fetchAll = (dataType) => {
 	return fetch(`http://localhost:3001/api/v1/${dataType}`)
 			.then(response => response.json())
 			.catch(error => console.log(error));
 }
+
+// fetch POST
+const postData = (url, newData) => {
+	console.log(newData)
+  return fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(newData),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }).then(response => {
+    if (!response.ok) {
+      throw new Error(`return status: ${JSON.stringify(response.json())}`);
+    } else {
+			return response.json()
+    }
+  });
+}
+
+export { fetchAll, postData }
